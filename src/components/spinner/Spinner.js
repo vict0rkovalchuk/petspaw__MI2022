cb9ea82990fc68ca8cf2a4388a01aa730225f147
Victor <vict02r05@gmail.com>
2022-10-02 15:38:41 +0300

    perf: change displaying of spinner on different themes

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -4,7 +4,6 @@ const Spinner = () => {
       xmlns="http://www.w3.org/2000/svg"
       style={{
         margin: 'auto',
-        background: 'rgb(255, 255, 255)',
         display: 'block',
         shapeRendering: 'auto'
       }}
